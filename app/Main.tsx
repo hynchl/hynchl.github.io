ead7235e6db33b8f1094aea0084f82e5a4e656b0
Hyunchul Kim <mail@hyunchu.kim>
2025-04-27 05:20:00 +0900

    fix build errors

--- app/Main.tsx
+++ app/Main.tsx
@@ -525,7 +525,7 @@ export default function Home({
           </Link>
         </p>
 
-        <div className="pr-20 xl:pr-32 py-12 dark:text-yellow-300">
+        <div className="py-12 pr-20 dark:text-yellow-300 xl:pr-32">
           <h2 className=" top-0 pt-1 text-xl">News</h2>
           <News />
         </div>
@@ -605,9 +605,20 @@ export default function Home({
                 mail: <a href="mailto:hyunchul.kim@kaist.ac.kr">hyunchul.kim@kaist.ac.kr</a> or{' '}
                 <a href="mailto:mail@hyunchul.kim">mail@hyunchul.kim </a>
               </li>
-              <li> <a href="https://www.linkedin.com/in/hynchl">linkedin: www.linkedin.com/in/hynchl </a> </li>
-              <li> <a href="https://instagram.com/hynchl"> instagram: hynchl </a> </li>
-              <li> <a href="https://x.com/kimhynchl"> twitter: kimhynchl </a> </li>
+              <li>
+                {' '}
+                <a href="https://www.linkedin.com/in/hynchl">
+                  linkedin: www.linkedin.com/in/hynchl{' '}
+                </a>{' '}
+              </li>
+              <li>
+                {' '}
+                <a href="https://instagram.com/hynchl"> instagram: hynchl </a>{' '}
+              </li>
+              <li>
+                {' '}
+                <a href="https://x.com/kimhynchl"> twitter: kimhynchl </a>{' '}
+              </li>
             </ul>
           </div>
         </div>
