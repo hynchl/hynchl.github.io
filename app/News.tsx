247a9d87fd3c3998bf22b104cd423d6aff88723b
Hyunchul Kim <30741095+hynchl@users.noreply.github.com>
2025-06-07 14:16:16 +0900

    fix padding

--- app/News.tsx
+++ app/News.tsx
@@ -6,9 +6,7 @@ export default function News() {
         <a href="https://cixschool2025.isir.upmc.fr/">Computational Interaction Summer School</a> in
         Paris, France!
       </ul>
-      <ul className="pb-2 pt-6">
-        • Attended CHI 2025 (April 28 – May 1, 2025) in Yokohama, Japan.
-      </ul>
+      <ul className="pb-2">• Attended CHI 2025 (April 28 – May 1, 2025) in Yokohama, Japan.</ul>
       <ul className="pb-2">
         • Gave a talk on large language models and online communities at Forkingroom 2025: Left Tech
         (April 16, 2025)
