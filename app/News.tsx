4e6a099580b7b92aea9cc2db30aee92326244487
Hyunchul Kim <30741095+hynchl@users.noreply.github.com>
2025-08-10 14:49:23 +0900

    update news

--- app/News.tsx
+++ app/News.tsx
@@ -3,18 +3,7 @@ import Link from '@/components/Link'
 export default function News() {
   return (
     <>
-      <ul className="pb-2 pt-6">
-        • My PhD proposal presentation will take place at 10 a.m. (KST) on August 7 in Room 2332,
-        Laughlin Hall (N5, 3rd Floor), KAIST. Remote attendance is available via{' '}
-        <Link
-          className="animate-shaking rounded-sm p-0.5 leading-8 tracking-tight text-yellow-400 hover:bg-yellow-300 hover:text-black"
-          href={'https://meet.google.com/czn-etan-qrb'}
-        >
-          {' '}
-          [google meet]
-        </Link>
-        .
-      </ul>
+      <ul className="pb-2 pt-6">• Completed my PhD proposal presentation (August 7, 2025).</ul>
       <ul className="pb-2">
         • Attended Summer School on{' '}
         <a href="https://cixschool2025.isir.upmc.fr/">Computational Interaction Summer School</a> in
