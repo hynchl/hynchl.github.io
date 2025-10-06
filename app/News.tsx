44f6715f9e4e229e92452ca82d5036ec57a55c21
Hyunchul Kim <mail@hyunchul.kim>
2025-10-06 19:37:14 +0900

    update (oct 6th, 2025)

--- app/News.tsx
+++ app/News.tsx
@@ -3,13 +3,22 @@ import Link from '@/components/Link'
 export default function News() {
   return (
     <>
-      <ul className="pb-2 pt-6">• Completed my PhD proposal presentation (August 7, 2025).</ul>
+      <ul className="pb-2 pt-6">
+        • Presenting at ISMAR 2025 (October 8–11, 2025) in Daejeon, South Korea — both in the
+        Doctoral Consortium and Poster sessions.
+      </ul>
+      <ul className="pb-2">
+        • Attended <strong>UIST 2025</strong> (September 28–October 1, 2025) in Busan, South Korea.
+      </ul>
+      <ul className="pb-2">• Completed my PhD proposal presentation (August 7, 2025).</ul>
       <ul className="pb-2">
         • Attended Summer School on{' '}
-        <a href="https://cixschool2025.isir.upmc.fr/">Computational Interaction Summer School</a> in
+        <a href="https://cixschool2025.isir.upmc.fr/">Computational interaction summer school</a> in
         Paris, France!
       </ul>
-      <ul className="pb-2">• Attended CHI 2025 (April 28 – May 1, 2025) in Yokohama, Japan.</ul>
+      <ul className="pb-2">
+        • Presented a full paper at CHI 2025 (April 28 – May 1, 2025) in Yokohama, Japan.
+      </ul>
       <ul className="pb-2">
         • Gave a talk on large language models and online communities at Forkingroom 2025: Left Tech
         (April 16, 2025)
